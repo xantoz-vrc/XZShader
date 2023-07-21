624066b66a48680b6c153f21e174ef4c22d62ae0
xantoz-vrc <xantoz-vrc@nowhere.com>
2023-07-21 17:38:55 +0200

    Create particle meshes with various different bounds
    So we do not have to use a SkinnedMeshRenderer to set the bounds to be
    suitable for whatever geometry shader we are doing.

--- ParticleMeshes/Editor/CreateParticlesMesh.cs
+++ ParticleMeshes/Editor/CreateParticlesMesh.cs
@@ -10,6 +10,7 @@ public class CreateParticlesMesh : MonoBehaviour
 {
     // Change to what sizes you need
     private static readonly int[] sizes = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 };
+    private static readonly int[] boundSizes = { 2, 3, 4, 5, 10 };
 
     [MenuItem("GameObject/Create Particles Triangle Mesh")]
     static void CreateTrianglesMesh()
@@ -41,6 +42,27 @@ public class CreateParticlesMesh : MonoBehaviour
             string path = "Assets/XZShader/ParticleMeshes/Points" + size + ".asset";
             AssetDatabase.CreateAsset(mesh, path);
             EditorGUIUtility.PingObject(mesh);
+        }
+    }
+
+    [MenuItem("GameObject/Create Particles Point Mesh with bounds")]
+    static void CreatePointMesh2()
+    {
+        foreach (int b in boundSizes) {
+            foreach (int size in sizes) {
+                var mesh = new Mesh();
+                mesh.vertices = new Vector3[size];
+                mesh.SetIndices(
+                    indices: new ushort[size],
+                    topology: MeshTopology.Points,
+                    submesh: 0,
+                    calculateBounds: false,
+                    baseVertex: 0);
+                mesh.bounds = new Bounds(new Vector3(0, 0, 0), new Vector3(b, b, b));
+                string path = "Assets/XZShader/ParticleMeshes/Points" + size + "-b" + b + "m.asset";
+                AssetDatabase.CreateAsset(mesh, path);
+                EditorGUIUtility.PingObject(mesh);
+            }
         }
     }
 }
