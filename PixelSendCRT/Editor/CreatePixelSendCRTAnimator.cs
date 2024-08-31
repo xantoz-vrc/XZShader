d4dbf825035c4b2655908a9b221e5c58e4092b24
xantoz-vrc <xantoz-vrc@nowhere.com>
2024-08-31 13:30:46 +0200

    Make the basic structure for creating the animator controllers etc. needed for avatar use

---
+++ PixelSendCRT/Editor/CreatePixelSendCRTAnimator.cs
@@ -0,0 +1,20 @@
+// idea by Nave, original: https://pastebin.com/Q43UPHf4
+#if UNITY_EDITOR
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreatePixelSendCRTAnimator : MonoBehaviour
+{
+    [MenuItem("GameObject/Generate Animator Controller for PixelSendCRT")]
+    static void CreateAnimatorController()
+    {
+        // string path = "Assets/XZShader/PixelSendCRT/PixelSendCRT Animator.controller";
+        // AssetDatabase.CreateAsset(controller, path);
+        // EditorGUIUtility.PingObject(controller);
+    }
+}
+
+#endif
