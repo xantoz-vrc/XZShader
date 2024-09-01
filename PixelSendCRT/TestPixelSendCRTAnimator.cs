d3eeedce7c6ab791dd1b79a67149b183a3d7e2a3
xantoz-vrc <xantoz-vrc@nowhere.com>
2024-09-01 19:42:23 +0200

    Start making a test rig to test that the animator controller works
    We have this before going all the way and making the avatar set up

---
+++ PixelSendCRT/TestPixelSendCRTAnimator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPixelSendCRTAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private float tickWait = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(WritePixel), 1.0f, tickWait);
+    }
+
+    void WritePixel()
+    {
+        Debug.Log("WritePixel");
+        animator.SetInteger("PixelSendCRT/V0", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/V1", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/V2", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/V3", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/V4", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/V5", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/V6", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/V7", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/V8", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/V9", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/VA", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/VB", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/VC", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/VD", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/VE", (int)(Random.value*255));
+        animator.SetInteger("PixelSendCRT/VF", (int)(Random.value*255));
+
+        bool CLK = !animator.GetBool("PixelSendCRT/CLK");
+        animator.SetBool("CLK", CLK);
+    }
+}
